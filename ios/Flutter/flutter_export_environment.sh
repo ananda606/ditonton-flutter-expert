f9fea22e811ec721cf3340a46d6e141008ac5942
ananda606 <85835149+ananda606@users.noreply.github.com>
2022-06-24 07:37:04 +0700

    test

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter expert\submisi 1 flutter expert - Copy"
+export "FLUTTER_APPLICATION_PATH=D:\flutter expert\ditonton-flutter-expert"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
