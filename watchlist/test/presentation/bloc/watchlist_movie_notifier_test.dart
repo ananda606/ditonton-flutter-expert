548e1b41d752238ca3c91cca0ed98646ae9f7f67
ananda606 <85835149+ananda606@users.noreply.github.com>
2022-06-25 09:07:34 +0700

    Update watchlist_movie_notifier_test.dart

--- watchlist/test/presentation/bloc/watchlist_movie_notifier_test.dart
+++ watchlist/test/presentation/bloc/watchlist_movie_notifier_test.dart
@@ -5,7 +5,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:bloc_test/bloc_test.dart';
-
 import '../../dummy_data/dummy_objects.dart';
 import 'watchlist_movie_notifier_test.mocks.dart';
 
